4f161bfa0437be1336244fdd63aa4149d3906cfa
TRT-gyl <3344855646@qq.com>
2024-12-13 09:26:24 +0000

    V1.7

--- pagesss/分布式电站.py
+++ pagesss/分布式电站.py
@@ -1,12 +1,8 @@
+import tempfile
 import os
-
 import streamlit as st
 import pandas as pd
 
-# 设置页面布局为 wide 模式
-st.set_page_config(layout="wide")
-
-# 定义 Streamlit 应用程序
 def main():
     st.title("分布式电站台账数据处理")
 
@@ -67,24 +63,24 @@ def main():
                 fill_value = st.text_input("请输入要填充的分布式电站名称", "河南主站驻马店城区分布式光伏电站")
 
                 if st.button("开始数据处理"):
+                    # 创建临时文件
+                    with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as tmp_file:
+                        output_file = tmp_file.name
 
                     # 将旧表的选定列数据复制到新表的选定列
                     for old_col, new_col in zip(old_columns, new_columns):
                         new_df[new_col] = old_df[old_col].copy()
-                        # 假设你要处理的列名为 '电压等级'
 
-                    column_name = '电压等级'
-                    column_name1 = '公共连接点电压等级'
-                    # 定义要替换的旧值和新值的映射字典
-                    replacement_dict = {
+                    # 替换特定值
+                    new_df['电压等级'] = new_df['电压等级'].replace({
                         '交流380V': '380V',
                         '交流220V': '220V',
                         '交流10kV': '10kV',
                         '交流35kV': '35kV',
                         'AC03802': '380V',
                         'AC02202': '220V',
-                    }
-                    replacement_dict1 = {
+                    })
+                    new_df['公共连接点电压等级'] = new_df['公共连接点电压等级'].replace({
                         '交流380V': '380V',
                         '交流220V': '220V',
                         '交流6kV': '6kV',
@@ -92,30 +88,16 @@ def main():
                         '交流20kV': '20kV',
                         '交流35kV': '35kV',
                         '交流66kV': '66kV'
-                    }
-
-                    # 替换特定值
-                    new_df[column_name] = new_df[column_name].replace(replacement_dict)
-                    new_df[column_name1] = new_df[column_name1].replace(replacement_dict1)
+                    })
 
                     # 检查条件并修改数据
-                    new_df.loc[(new_df['电压等级'].notnull()) & (
-                        new_df['分布式电站名称'].isnull()), '分布式电站名称'] = fill_value
+                    new_df.loc[(new_df['电压等级'].notnull()) & (new_df['分布式电站名称'].isnull()), '分布式电站名称'] = fill_value
 
-                    # 将更新后的新表写入新的 Excel 文件
-                    output_file = new_file.name
-                    # 检查 output_file 是否存在，如果不存在则复制 new_file 到 output_file
-                    if not os.path.exists(output_file):
-                        with open(output_file, "wb") as f:
-                            f.write(new_file.getbuffer())
-                    # 使用 ExcelWriter 追加数据
-                    with pd.ExcelWriter(output_file, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-                        # 手动计算 startrow，确保从最后一个非空行的下一行开始写入
-                        startrow = writer.sheets['Sheet1'].max_row if writer.sheets['Sheet1'].max_row > 0 else 0
-                        new_df.to_excel(writer, sheet_name='Sheet1', index=False, startrow=startrow, header=False)
+                    # 将更新后的新表写入临时文件
+                    new_df.to_excel(output_file, index=False)
 
-                    st.success(f"更新后的新表已保存到 {output_file}")
-                    df3 = pd.read_excel(output_file, engine='openpyxl')
+                    st.success(f"更新后的新表已保存到临时文件 {output_file}")
+                    df3 = pd.read_excel(output_file)
                     # 显示最终的 DataFrame
                     st.write("处理完成的文件内容：")
                     st.dataframe(df3)
@@ -123,10 +105,9 @@ def main():
                         btn = st.download_button(
                             label="下载更新后的新表",
                             data=f,
-                            file_name=output_file,
+                            file_name=output_file.split('/')[-1],
                             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                         )
-
             else:
                 st.error("原始数据表和新模板表选择的列数必须相同。")
         else:
